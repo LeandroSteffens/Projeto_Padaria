b836b72b598df8d3c1e950ef9faf4ef48b5cc6c0
Leandro Steffens de Oliveira <lenhn1007mt@gmail.com>
2022-05-17 17:44:31 -0400

    v1

--- cadastro_auto.h
+++ cadastro_auto.h
@@ -108,11 +108,280 @@ void cadastros(){
     aux = 1;
     id_produto[aux].id = aux;
     id_produto[aux].vazio = 1;
-    strcpy(id_produto[aux].descricao, "pao frances");
+    strcpy(id_produto[aux].descricao, "Pao frances");
     id_produto[aux].estoque_min = 2;
     id_produto[aux].qnt_estoque = 5;
     id_produto[aux].valor_custo = 1.5;
     id_produto[aux].lucro = 50;
     id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Pao de forma");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Pao arabe");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Pao caseiro");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Sonho");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Empada");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Brigadeiro");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Beijinho");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Risolis");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Misto quente");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Cafe");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Achocolatado");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Coca cola");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Guarana");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Agua mineral");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Agua com gas");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Trident");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Pirulito");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Queijo mussarela");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    id_produto[aux].id = aux;
+    id_produto[aux].vazio = 1;
+    strcpy(id_produto[aux].descricao, "Presunto");
+    id_produto[aux].estoque_min = 2;
+    id_produto[aux].qnt_estoque = 5;
+    id_produto[aux].valor_custo = 1.5;
+    id_produto[aux].lucro = 50;
+    id_produto[aux].valor_venda = id_produto[aux].valor_custo + id_produto[aux].valor_custo * (id_produto[aux].lucro/100);
+
+    aux++;
+
+    //fornecedores
+    aux = 1;
+
+    id_fornecedor[aux].id = aux;
+    id_fornecedor[aux].vazio = 1;
+    strcpy(id_fornecedor[aux].nome, "cesao");
+    strcpy(id_fornecedor[aux].cnpj, "00688888888");
+    strcpy(id_fornecedor[aux].endereco, "ufmt");
+    strcpy(id_fornecedor[aux].telefone, "6599999999");
+    strcpy(id_fornecedor[aux].ins_est, "1111111");
+    strcpy(id_fornecedor[aux].pessoa_contato, "lex");
+
+    aux++;
+
+    id_fornecedor[aux].id = aux;
+    id_fornecedor[aux].vazio = 1;
+    strcpy(id_fornecedor[aux].nome, "leozao");
+    strcpy(id_fornecedor[aux].cnpj, "00688888888");
+    strcpy(id_fornecedor[aux].endereco, "ufmt");
+    strcpy(id_fornecedor[aux].telefone, "6599999999");
+    strcpy(id_fornecedor[aux].ins_est, "1111111");
+    strcpy(id_fornecedor[aux].pessoa_contato, "lex");
+
+    aux++;
+
+    id_fornecedor[aux].id = aux;
+    id_fornecedor[aux].vazio = 1;
+    strcpy(id_fornecedor[aux].nome, "cr7zao");
+    strcpy(id_fornecedor[aux].cnpj, "00688888888");
+    strcpy(id_fornecedor[aux].endereco, "ufmt");
+    strcpy(id_fornecedor[aux].telefone, "6599999999");
+    strcpy(id_fornecedor[aux].ins_est, "1111111");
+    strcpy(id_fornecedor[aux].pessoa_contato, "lex");
+
+    aux++;
+
+    id_fornecedor[aux].id = aux;
+    id_fornecedor[aux].vazio = 1;
+    strcpy(id_fornecedor[aux].nome, "messao");
+    strcpy(id_fornecedor[aux].cnpj, "00688888888");
+    strcpy(id_fornecedor[aux].endereco, "ufmt");
+    strcpy(id_fornecedor[aux].telefone, "6599999999");
+    strcpy(id_fornecedor[aux].ins_est, "1111111");
+    strcpy(id_fornecedor[aux].pessoa_contato, "lex");
+
+    aux++;
+
+    id_fornecedor[aux].id = aux;
+    id_fornecedor[aux].vazio = 1;
+    strcpy(id_fornecedor[aux].nome, "rock balboa");
+    strcpy(id_fornecedor[aux].cnpj, "00688888888");
+    strcpy(id_fornecedor[aux].endereco, "ufmt");
+    strcpy(id_fornecedor[aux].telefone, "6599999999");
+    strcpy(id_fornecedor[aux].ins_est, "1111111");
+    strcpy(id_fornecedor[aux].pessoa_contato, "lex");
+
+    aux++;
 
 }
